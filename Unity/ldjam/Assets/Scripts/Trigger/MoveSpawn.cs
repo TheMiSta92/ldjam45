4930f1031cd514be6935eb5e26d9e640fc961ee6
Michael Staudinger, MSc <work@michael-staudinger.at>
2019-10-07 23:06:52 +0200

    Added additional spawns

---
+++ Unity/ldjam/Assets/Scripts/Trigger/MoveSpawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSpawn : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        float oldSpawnX = GameObject.FindGameObjectWithTag("Player").transform.parent.position.x;
+        float newSpawnX = this.gameObject.transform.position.x;
+        GameObject.FindGameObjectWithTag("Player").transform.parent.Translate(newSpawnX - oldSpawnX, 0f, 0f);
+        GameObject.FindGameObjectWithTag("Player").transform.Translate(oldSpawnX - newSpawnX, 0f, 0f);
+        this.gameObject.GetComponent<Collider>().enabled = false;
+    }
+
+}
