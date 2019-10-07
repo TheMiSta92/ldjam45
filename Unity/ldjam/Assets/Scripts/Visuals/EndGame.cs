ec22e609fd2fd37cd4cbd2a0221b8bfb75430a6d
AtzederSerbe <AtzederSerbe@users.noreply.github.com>
2019-10-07 15:55:24 +0200

    Added FadeOut on Game End

---
+++ Unity/ldjam/Assets/Scripts/Visuals/EndGame.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField][Range(0.001f,0.5f)] private float fadeOutVelocity;
+    private bool isEndTriggered;
+    private MeshRenderer mesh;
+    private bool firstWhite=true;
+
+    private void Update()
+    {
+        if (isEndTriggered)
+        {
+            if (mesh.material.color.a < 1)
+            {
+                Color col = mesh.material.color;
+                if (mesh.material.color.a + fadeOutVelocity > 1)
+                {
+                    col.a = 1f;
+                    mesh.material.color = col;
+                    if (firstWhite)
+                    {
+                        firstWhite = false;
+                        StartCoroutine(PlayCreditsAndGoToStart());
+                    }
+                }
+                col.a = col.a+fadeOutVelocity;
+                mesh.material.color = col;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+           collision.gameObject.GetComponent<PlayerMovement>().enabled = false;
+            mesh = GameObject.FindGameObjectWithTag("Fader").GetComponent<MeshRenderer>();
+            isEndTriggered = true;
+        }
+    }
+    private IEnumerator PlayCreditsAndGoToStart()
+    {
+        yield return new WaitForSeconds(1f);
+        //play Credits
+        yield return new WaitForSeconds(30f);
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ Unity/ldjam/Assets/Scripts/Visuals/InitFadeOutObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitFadeOutObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color init = gameObject.GetComponent<MeshRenderer>().material.color;
+        init.r = 1f;
+        init.g = 1f;
+        init.b = 1f;
+        init.a = 0;
+        gameObject.GetComponent<MeshRenderer>().material.color = init;
+    }
+}
