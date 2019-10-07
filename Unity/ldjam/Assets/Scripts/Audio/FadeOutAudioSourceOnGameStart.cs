70b2ee0d9d3949dfdaaad4d5f8edbd9e090a9bd2
Michael Staudinger, MSc <work@michael-staudinger.at>
2019-10-07 19:03:59 +0200

    Added Title Screen music (fades out on GameStart)

---
+++ Unity/ldjam/Assets/Scripts/Audio/FadeOutAudioSourceOnGameStart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutAudioSourceOnGameStart : MonoBehaviour {
+
+    [SerializeField] [Range(.3f, 5f)] protected float fadeTime = 1f;
+
+    protected bool doFade = false;
+    protected float currentVol = 1f;
+    protected float currentTime = 0f;
+
+
+
+    private void Start() {
+        sGameEventManager.Access().OnGameStart += fadeOut;
+    }
+
+    private void Update() {
+        if (this.doFade) {
+            this.currentTime += Time.deltaTime;
+            float vol = 1f - this.currentTime / this.fadeTime;
+            if (vol <= 0f) {
+                this.doFade = false;
+                vol = 0f;
+            }
+            this.gameObject.GetComponent<AudioSource>().volume = vol;
+        }
+    }
+
+
+
+    private void fadeOut() {
+        this.doFade = true;
+        this.currentTime = 0f;
+    }
+
+}
